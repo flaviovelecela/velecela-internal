fe9d29072717ee9236ec9cf21170b1e0b080419b
flaviovelecela <velecelaf@usf.edu>
2024-04-28 23:30:12 -0400

    react additions and changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import HomePage from './Components/landing-page/landing-page';
+import Navbar from './Components/navigation-page/navigation-page';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='*' element={<HomePage />} />
+          <Route path='/' exact={true} element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/landing-page/landing-page.js
+++ src/Components/landing-page/landing-page.js
@@ -1,5 +1,17 @@
-return {
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', margin: '50px' }}>
+      <h1>Welcome to Upstream</h1>
+      <div style={{ margin: 'auto', maxWidth: '600px' }}>
+      <p>
+      Upstream is an game organizational tool designed to simplify how you manage and sort your video game collection. Upstream allows gamers to effortlessly organize their games across multiple platforms in one centralized location. Whether you're looking to categorize your games by completion, achivements, or even dropped games. Upstream makes it possible with just a few clicks. The best part? Upstream is available to use right now, offering a seamless and hassle-free setup process. Say goodbye to the clutter and hello to a more organized gaming experience with Upstream. Start optimizing your gaming library today and enjoy a more streamlined and enjoyable gaming experience.
+      </p>
+      </div>
+    </div>
+  );
 }
 
-export default landing-page;
+export default HomePage;
