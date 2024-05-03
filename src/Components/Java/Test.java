894ac5b1704ed8debd338c8a01bd4658bbe26109
Ace010 <rafaelvelecela15@gmail.com>
2024-05-03 15:15:17 -0400

    hello mr bombtasiticajklasohas;j;ga

---
+++ src/Components/Java/Test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Test
+{
+    public static void main(String args[])
+    {
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Please Enter An Integral");
+
+        String integral_string = user_input.nextLine();
+        System.out.println("Integral entered is: "+ integral_string);
+    }
+
+    public static int integral_calculator()
+    {
+
+
+        
+    }
+}
