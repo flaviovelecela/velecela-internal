ffa8ed3abd2d030f36957e237528f64814c22865
flaviovelecela <Velecelaf@usf.edu>
2024-05-03 17:53:40 -0400

    started using maven springboot

--- src/Components/landing-page/landing-page.js
+++ src/Components/landing-page/landing-page.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function LandingPage() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/data');
+        setData(response.data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  })
+
   return (
     <div style={{ textAlign: 'center', margin: '50px' }}>
       <h1>Welcome!</h1>
