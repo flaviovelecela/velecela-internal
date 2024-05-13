26938270a2eb52f5695877a2a1643093a582bdff
flaviovelecela <Velecelaf@usf.edu>
2024-05-13 15:17:06 -0400

    GUI changes

--- src/Components/navigation-page/navigation.js
+++ src/Components/navigation-page/navigation.js
@@ -34,6 +34,10 @@ function navigation() {
               <Link className="nav-link disabled" to="#" aria-disabled="true">Disabled</Link>
             </li>
           </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
         </div>
       </div>
     </nav>
